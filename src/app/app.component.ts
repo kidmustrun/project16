553213063979da1a4207a7d594cc50a8706c6b99
Irina <parakerta@mail.ru>
2020-12-18 22:13:46 +0300

    rest api

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
+import { DatebaseService } from 'src/app/shared/datebase.service';
+
 import {
   MyWorker,
-  MyWorkersDatabase,
   MyWorkerType,
 } from './shared/worker.model';
 
@@ -9,21 +10,34 @@ import {
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
+  providers: [DatebaseService]
 })
 export class AppComponent {
   title = 'Список сотрудников';
-  workers: MyWorker[] = MyWorkersDatabase;
+  workers: MyWorker[];
   myWorkerType = MyWorkerType;
+  constructor(private DatebaseService: DatebaseService){}
+  ngOnInit(){
+         this.DatebaseService.getData().subscribe((data:MyWorker[]) => this.workers=data);
+         
+}
 
   getByType(type: number) {
+    if (this.workers)
     return this.workers.filter((worker) => worker.type === type);
+
   }
 
-  onDeleteById(id: number) {
+  onDeleteById(id: number|string) {
     let index = this.workers.findIndex((worker) => worker.id === id);
+    let idForRequest:string = String(this.workers[index].id);
+
     if (index !== -1) {
       this.workers.splice(index, 1);
+      this.DatebaseService.deleteData(idForRequest).subscribe((data) => idForRequest=data);
+      
     }
+    
   }
 
   onAddWorker(worker) {
@@ -35,11 +49,18 @@ export class AppComponent {
     worker.id = id;
    
     this.workers.push(worker);
+    this.DatebaseService.postData(worker).subscribe((data) => worker=data);
+     
+  
+    
+  
+    
   }
   onEditWorker(worker_edit){
     let index = this.workers.findIndex((worker) => worker.id === worker_edit.id);
     if (index !== -1) {
       this.workers.splice(index, 1, worker_edit);
+      this.DatebaseService.putData(worker_edit).subscribe((data) => worker_edit=data);
     }
   }
  
