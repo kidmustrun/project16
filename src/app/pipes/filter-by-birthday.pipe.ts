30c3483127307cd9e3fc8d546dc5750b125b3d2f
Irina <parakerta@mail.ru>
2020-12-22 18:27:33 +0300

    little fix

--- src/app/pipes/filter-by-birthday.pipe.ts
+++ src/app/pipes/filter-by-birthday.pipe.ts
@@ -6,13 +6,13 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class FilterByBirthdayPipe implements PipeTransform {
 
   transform(workers,sorting) {
-    if(sorting>0)
-    return workers.sort((a, b) => { a = new Date(a.birthday);
-      b = new Date(b.birthday);
-      return a>b ? -1 : a<b ? 1 : 0;})
-    else if(sorting=0) return workers.sort((a, b) => { a = new Date(a.birthday);
-      b = new Date(b.birthday);
-      return a>b ? 1 : a<b ? -1 : 0;})
+    // if(sorting>0)
+    // return workers.sort((a, b) => { a = new Date(a.birthday);
+    //   b = new Date(b.birthday);
+    //   return a>b ? -1 : a<b ? 1 : 0;})
+    // else if(sorting=0) return workers.sort((a, b) => { a = new Date(a.birthday);
+    //   b = new Date(b.birthday);
+    //   return a>b ? 1 : a<b ? -1 : 0;})
   }
 
 }
